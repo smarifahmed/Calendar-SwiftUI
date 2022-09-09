f15893b67f37d4521d62895ec9d620e008f5ea7e
Arif Ahmed <smarifahmed42@gmail.com>
2022-09-09 21:35:38 +0600

    Initial Commit

---
+++ Calendar-SwiftUI/Calendar_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calendar_SwiftUIApp.swift
+//  Calendar-SwiftUI
+//
+//  Created by SM Arif Ahmed on 9/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Calendar_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
